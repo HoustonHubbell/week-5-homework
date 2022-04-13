191e5c8a440770ce25256c4a886465e6cfa49e3d
HoustonHubbell <100814748+HoustonHubbell@users.noreply.github.com>
2022-04-13 15:17:11 -0500

    Add files via upload

--- homework.sql
+++ homework.sql
@@ -1,28 +1,73 @@
-CREATE TABLE IF NOT EXISTS customer(
-	customer_id SERIAL PRIMARY KEY,
-	customer_age INTEGER
+-- Question 1:
+select customer.first_name, customer.last_name, customer.email, city, district
+from customer
+full join address
+on customer.address_id = address.address_id
+full join city
+on address.city_id = city.city_id
+full join country
+on city.country_id = country.country_id
+where district = 'Texas';
+
+--Question 2:
+select customer.customer_id, customer.first_name, customer.last_name, amount
+from customer
+full join payment
+on customer.customer_id = payment.customer_id
+where amount > 6.99;
+
+
+--Question 3:
+select store_id, first_name, last_name
+from customer
+where customer_id in (
+	SELECT customer_id
+	from payment
+	group by customer_id
+	having sum(amount) > 175.00
+	order by sum(amount) desc
+)
+group by store_id, first_name, last_name;
+
+
+-- Question 4:
+select customer.first_name, customer.last_name, customer.email, city, country
+from customer
+full join address
+on customer.address_id = address.address_id
+full join city
+on address.city_id = city.city_id
+full join country
+on city.country_id = country.country_id
+where country = 'Nepal';
+
+--Question 5:
+select count(payment_id), staff.staff_id, staff.first_name, staff.last_name
+from staff
+full join payment
+on staff.staff_id = payment.staff_id
+group by staff.staff_id
+order by count(payment_id) desc;
+
+--Question 6:
+select all *
+from film;
+
+select rating, count(rating)
+from film
+group by rating
+order by count(rating)desc;
+
+-- Question 7:
+select customer_id, first_name, last_name
+from customer
+where customer_id in (
+	select customer_id
+	from payment
+	where amount > 6.99
 );
 
-CREATE TABLE IF NOT EXISTS movie(
-	movie_name VARCHAR(50) PRIMARY KEY,
-	theatre_id INTEGER,
-	total_tickets INTEGER,
-	movie_time DATE DEFAULT CURRENT_DATE,
-);
-
-CREATE TABLE IF NOT EXISTS ticket(
-	seat_id SERIAL PRIMARY KEY,
-	customer_id INTEGER,
-	movie_name VARCHAR(50),
-	FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
-	FOREIGN KEY(movie_name) REFERENCES movie(movie_name)
-);
-
-CREATE TABLE IF NOT EXISTS concession(
-	transaction_id SERIAL PRIMARY KEY,
-	inventory_id INTEGER,
-	transaction_amount NUMERIC(5,2),
-	customer_id INTEGER,
-	FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
-);
-
+--Question 8:
+select count(amount)
+from payment
+where amount = 0.00;
